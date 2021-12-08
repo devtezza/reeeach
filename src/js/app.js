b538ad4483634283f679acfc67be179ddc968dce
nandotezza <fernando.tezza@gmail.com>
2021-12-07 23:19:49 -0300

    Add menu toggler for mobile layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
 const menuBtn = document.querySelector('.menu-btn');
+const navWrapper = document.querySelector('.nav-wrapper');
+
+
 
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     menuBtn.classList.toggle('active');
+    navWrapper.classList.toggle('active');
 }
