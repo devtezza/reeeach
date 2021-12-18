fd72f08c7f7139aad328664bb81efaa626708e83
nandotezza <fernando.tezza@gmail.com>
2021-12-18 12:51:55 -0300

    Update scroll triggered animations on all sections to match media device

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ const itemLinkFooter = document.querySelectorAll('.item-link-footer');
 const itemLinkButton = document.querySelectorAll('.item-link-button');
 const allLinks = [itemLink, itemLinkFooter, itemLinkButton];
 const testimonial = document.querySelectorAll('.testimonial');
+const feature = document.querySelectorAll('.feature');
 
 // Call the function watchLinks to close mobile menu (if opened) when a link is clicked
 watchLinks(allLinks);
@@ -84,7 +85,7 @@ tlFeatures = gsap.timeline({
     scrollTrigger: {
         trigger: ".features",
         // markers: true,
-        start: "top center",
+        start: "10% center",
         end: "bottom center",
     }
 });
@@ -101,37 +102,31 @@ tlTestimonials = gsap.timeline({
 tlCtaSection = gsap.timeline({
     scrollTrigger: {
         trigger: ".cta-section",
-        markers: true,
+        // markers: true,
         start: "top center",
         end: "bottom center",
     }
 });
 
-
+tlFooter = gsap.timeline({
+   scrollTrigger: {
+    trigger: "footer",
+    // markers: true,
+    start: "20% 80%",
+    end: "bottom center",
+   }
+});
 
 
 ScrollTrigger.matchMedia({
-    // only for desktop and CTA Section
+    // desktop devices
     "(min-width: 1024px)": () => {
-        tlCtaSection.from(".cta-section .cta-img", {x: 3000, rotation: 360, duration: 2, ease:"power4.out"});
-    },
-
-    // all devices and sections
-    "all": () => {
-        // Animations for in tlHeader
-        tlHeader.from("h1", {x: -1000, duration: 2, ease: "power4.inOut"});
-        tlHeader.from(".hero-img", {x: 1000, duration: 1.5, ease: "back.out(2)"});
-        tlHeader.from(".top-bar", {opacity: 0, duration: 2, ease: "power4.inOut"});
-
         // Animations in tlAbout (div -> Intro)
-        tlAbout.from(".intro h2", {x: 3000, duration: 1, ease: "power4.out"});
-        tlAbout.from(".intro p", {x: 3000, duration: 2, ease: "power4.out"});
-        tlAbout.from(".intro .cta-btn", {x: 3000, duration: 0.1, ease: "power4.out"});
+        tlAbout.from(".intro", {x: 3000, duration: 1.5, ease: "power4.out"});        
 
         // Animations for tlAbout (div -> How It Works)
-        tlAbout.from(".how-it-works h2", {x: -3000, duration: 2, ease: "power4.out"});
-        tlAbout.from(".how-it-works ul", {x: -3000, duration: 3, ease: "power4.out"});
-
+        tlAbout.from(".how-it-works", {x: -3000, duration: 1.5, ease: "power4.out"});
+       
         // Animations for tlFeatures
         tlFeatures.from(".features h2", {x: 3000, duration: 2, ease: "power4.out"});
         tlFeatures.from(".feature", {scale: 0, duration: 3, ease: "power2.out", stagger: 0.7});
@@ -139,13 +134,56 @@ ScrollTrigger.matchMedia({
 
         // Animations for tlTestimonials
         tlTestimonials.from(".testimonials h2", {x: -3000, duration: 2, ease:"power4.out"});
-        tlTestimonials.from(testimonial[0], {x: -3000, rotation: 360, duration: 1.5, ease: "power4.out"});
-        tlTestimonials.from(testimonial[2], {x: 3000, rotation: 360, duration: 1.5, ease: "power4.out"});
-        tlTestimonials.from(testimonial[1], {scale: 0, duration: 2, ease: "power4.out"})
+        tlTestimonials.from(testimonial[0], {x: -3000, duration: 1.5, ease: "power4.out"});
+        tlTestimonials.from(testimonial[2], {x: 3000, duration: 1.5, ease: "power4.out"});
+        tlTestimonials.from(testimonial[1], {scale: 0, duration: 2, ease: "power4.out"});
+
+        // Animations for CTA Section
+        tlCtaSection.from(".cta-section .cta-img", {x: 3000, rotation: 360, duration: 2, ease:"power4.out"});
+    },
+
+    // mobile devices
+    "(max-width: 1023px)": () => {
+        // Animations in tlAbout (div -> Intro)
+        tlAbout.from(".intro h2", {x: 3000, duration: 1, ease: "power4.out"});
+        tlAbout.from(".intro p", {x: 3000, duration: 2, ease: "power4.out"});
+        tlAbout.from(".intro .cta-btn", {x: 3000, duration: 0.1, ease: "power4.out"});
+ 
+        // Animations for tlAbout (div -> How It Works)
+        tlAbout.from(".how-it-works h2", {x: -3000, duration: 2, ease: "power4.out"});
+        tlAbout.from(".how-it-works ul", {x: -3000, duration: 2, ease: "power4.out"});
+ 
+        // Animations for tlFeatures
+        tlFeatures.from(".features h2", {x: 3000, duration: 2, ease: "power4.out"});
+        tlFeatures.from(feature[0], {scale: 0, duration: 1.5, ease: "power2.out"});
+        tlFeatures.from(feature[1], {scale: 0, duration: 1.5, ease: "power2.out"});
+        tlFeatures.from(feature[2], {scale: 0, duration: 1.5, ease: "power2.out"});
+        tlFeatures.from(feature[3], {scale: 0, duration: 1.5, ease: "power2.out"}); 
+        tlFeatures.from(".features .cta-btn", {x: -3000, duration: 0.1, ease: "power4.out"});
+ 
+        // Animations for tlTestimonials
+        tlTestimonials.from(".testimonials h2", {x: -3000, duration: 2, ease:"power4.out"});
+        tlTestimonials.from(testimonial[0], {x: 3000, duration: 1.5, ease: "power4.out"});
+        tlTestimonials.from(testimonial[1], {x: -3000, duration: 1.5, ease: "power4.out"});
+        tlTestimonials.from(testimonial[2], {x: 3000, duration: 1.5, ease: "power4.out"});
+    },
+
+    // all devices
+    "all": () => {
+        // Animations for in tlHeader
+        tlHeader.from("h1", {x: -1000, duration: 2, ease: "power4.inOut"});
+        tlHeader.from(".hero-img", {x: 1000, duration: 1.5, ease: "back.out(2)"});
+        tlHeader.from(".top-bar", {opacity: 0, duration: 1, ease: "power4.inOut"});
 
         // Animations for tlCtaSection
         tlCtaSection.from(".cta-section h2", {x: 3000, duration: 2, ease:"power4.out"});
-        tlCtaSection.from(".cta-section .cta-btn", {x: -3000, duration: 0.2, ease:"power2.out"});
+        tlCtaSection.from(".cta-section .cta-btn", {x: -3000, duration: 0.2, ease:"power2.out"});        
+        
+        // Animations for Footer
+        tlFooter.from("footer .logo", {scale: 0, duration: 1, ease: "power4.out"});
+        tlFooter.from("footer .newsletter", {scale: 0, duration: 1, ease: "power4.out"});
+        tlFooter.from(".nav-footer", {scale: 0, duration: 1, ease: "power4.out"});
+        tlFooter.from(".social-icons", {scale: 0, duration: 1, ease: "power4.out"});
+        tlFooter.from(".attribution", {scale: 0, duration: 1.5, ease: "power4.out"});
     }
 });
-
